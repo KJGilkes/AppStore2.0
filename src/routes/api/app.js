153dc71d323a92b22e33dcd632320141ccc52083
KJGilkes <khalilgilkes@Khalils-MacBook-Pro.local>
2016-08-07 16:28:30 -0400

    Refactored app routes and user routes also added new paths for crud capabilities

--- src/routes/api/app.js
+++ src/routes/api/app.js
@@ -1,73 +1,55 @@
+const App = require('../../models/AppModel');
+
 module.exports = function(express){
   var router = express.Router();
 
-  //Route that shows all of the apps
+  //Route that shows all of the Apps
   router.get('/apps', function(req,res){
-    res.json([{
-      "id": "0",
-      "title": "Shooterific",
-      "description": "Shoot em up action filled thriller",
-      "artAssets": [
-        { "title": "Splash Screen", "srcLink": "http://i.imgur.com/5e5Ihb6.jpg" },
-        { "title": "Cut Scene", "srcLink": "http://i.imgur.com/QQ3O6PO.jpg" }
-      ],
-      "releaseDate": "2016-06-15T22:29:20.000Z",
-      "createdAt": "2016-05-15T22:29:20.000Z",
-      "updatedAt": "2016-05-15T22:29:20.000Z",
-      "user": {
-        "id": "2",
-        "name": "Gilkes"
-      }
-    },
-    {
-      "id": "1",
-      "title": "Splash mountain",
-      "description": "Solve puzzles as you race against the tide",
-      "artAssets": [
-        { "title": "Splash Screen", "srcLink": "http://i.imgur.com/5e5Ihb6.jpg" },
-        { "title": "Cut Scene", "srcLink": "http://i.imgur.com/QQ3O6PO.jpg" }
-      ],
-      "releaseDate": "2016-06-15T22:29:20.000Z",
-      "createdAt": "2016-05-15T22:29:20.000Z",
-      "updatedAt": "2016-05-15T22:29:20.000Z",
-      "user": {
-        "id": "3",
-        "name": "Khalil"
-      }
-    }]);
-  })
+    App.all( (err) => {
+      res.status(500).json(err);
+    }, (data) => {
+      res.status(200).json(data);
+    })
+  });
 
-  // Route that shows a specific app by id
+  //Route that creates an App
+  router.post('/apps', (req,res) => {
+      App.add(req.body, (err) => {
+        res.status(500).json(err);
+      }, (data) => {
+        res.status(200).json(data);
+      })
+  });
+
+  //Route that shows a specific App
   router.get('/apps/:id', function(req,res){
-    if(req.params.id == 0){
-      res.json({
-        "id": "0",
-        "title": "Shooterific",
-        "description": "Shoot em up action filled thriller",
-        "artAssets": [
-          { "title": "Splash Screen", "srcLink": "http://i.imgur.com/5e5Ihb6.jpg" },
-          { "title": "Cut Scene", "srcLink": "http://i.imgur.com/QQ3O6PO.jpg" }
-        ],
-        "releaseDate": "2016-06-15T22:29:20.000Z",
-        "createdAt": "2016-05-15T22:29:20.000Z",
-        "updatedAt": "2016-05-15T22:29:20.000Z"
-      });
-    }else {
-      res.json({
-        "id": "1",
-        "title": "Splash Maze",
-        "description": "Solve puzzles as you race against the tide",
-        "artAssets": [
-          { "title": "Splash Screen", "srcLink": "http://i.imgur.com/5e5Ihb6.jpg" },
-          { "title": "Cut Scene", "srcLink": "http://i.imgur.com/QQ3O6PO.jpg" }
-        ],
-        "releaseDate": "2016-06-15T22:29:20.000Z",
-        "createdAt": "2016-05-15T22:29:20.000Z",
-        "updatedAt": "2016-05-15T22:29:20.000Z"
-      });
-    }
+    req.body.id = req.params.id;
+    App.one(req.body, (err) => {
+      res.status(500).json(err);
+    }, (data) => {
+      res.status(200).json(data);
+    })
+  });
 
-  })
+  //Route that updates a specific App
+  router.post('/apps/:id', function(req,res){
+    req.body.id = req.params.id;
+    App.update(req.body, (err) => {
+      res.status(500).json(err);
+    }, (data) => {
+      res.status(200).json(data);
+    });
+  });
+
+  //Route that deletes a specific App
+  router.delete('/apps/:id', function(req,res){
+    req.body.id = req.params.id;
+    App.remove(req.body, (err) => {
+      res.status(500).json(err);
+    }, (data) => {
+      res.status(200).json(data);
+    })
+  });
 
   return router;
 }
--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -1,39 +1,55 @@
+const User = require('../../models/UserModel');
+
 module.exports = function(express){
   var router = express.Router();
 
-  //Route that shows all of the users
+  //Route that shows all of the Users
   router.get('/users', function(req,res){
-    res.json([{
-      "user": {
-        "id": "2",
-        "name": "Gilkes"
-      }
-    },{
-      "user": {
-        "id": "3",
-        "name": "Khalil"
-      }
-    }]);
-  })
+    User.all( (err) => {
+      res.status(500).json(err);
+    }, (data) => {
+      res.status(200).json(data);
+    })
+  });
 
-  // Route that shows a specific user by id
+  //Route that creates a User
+  router.post('/users', (req,res) => {
+      User.add(req.body, (err) => {
+        res.status(500).json(err);
+      }, (data) => {
+        res.status(200).json(data);
+      });
+  });
+
+  //Route that shows a specific User
   router.get('/users/:id', function(req,res){
-    if(req.params.id == 2){
-      res.json({
-      "user": {
-        "id": "2",
-        "name": "Gilkes"
-        }
-      })
-    }else {
-      res.json(
-          {
-          "id": "3",
-          "name": "Khalil"
-          }
-        )
-      }
-  })
+    req.body.id = req.params.id;
+    User.one(req.body, (err) => {
+      res.status(500).json(err);
+    }, (data) => {
+      res.status(200).json(data);
+    })
+  });
+
+  //Route that updates a specific User
+  router.post('/users/:id', function(req,res){
+    req.body.id = req.params.id;
+    User.update(req.body, (err) => {
+      res.status(500).json(err);
+    }, (data) => {
+      res.status(200).json(data);
+    })
+  });
+
+  //Route that deletes a specific User
+  router.delete('/users/:id', function(req,res){
+    req.body.id = req.params.id;
+    User.remove(req.body, (err) => {
+      res.status(500).json(err);
+    }, (data) => {
+      res.status(200).json(data);
+    })
+  });
 
   return router;
 }
