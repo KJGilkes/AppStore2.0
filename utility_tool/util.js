726984d64aacc7a3348630544ed00549f5f4a6bd
KJGilkes <khalilgilkes@Khalils-MacBook-Pro.local>
2016-08-11 23:27:08 -0400

    made utility debug error messages a different color

--- utility_tool/util.js
+++ utility_tool/util.js
@@ -1,12 +1,20 @@
 const fs = require('fs');
-exports.log = (msg,obj) => {
+const color = require('colors');
+exports.debug = (msg, status) => {
+  const seperator = '\n=============================\n';
     if(process.env.DEBUG){
-      if(obj == undefined){
-        console.log(msg);
+      const coloredmsg = color.green(msg);
+      const errormsg = color.yellow(msg);
+      if(status == undefined){
+        console.log(coloredmsg + seperator);
+        fs.appendFile('logs/output.log', msg, () => {
+        });
+      }else if (status == 444) {
+        console.log(errormsg + seperator);
         fs.appendFile('logs/output.log', msg, () => {
         });
       }else{
-        console.log(msg,obj);
+        console.log(coloredmsg, status + seperator);
         fs.appendFile('logs/output.log', msg, () => {
         });
       }
